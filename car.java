bfb9731acc6e66826247a8f544132c2f3bc34bb0
s33127 <s33127@pjwstk.edu.pl>
2025-04-17 10:40:55 +0200

    zad1.3

--- car.java
+++ car.java
@@ -5,4 +5,14 @@ public class car extends vehicle {
     public void getNumberOfSeats() {
         System.out.println("Number of Seats in Car: " + numberOfSeats);
     }
+    @Override
+    public void start(){
+        super.start();
+        System.out.println("Strating Car");
+    }
+    @Override
+    public void stop(){
+        super.stop();
+        System.out.println("Stoping Car");
+    }
 }
