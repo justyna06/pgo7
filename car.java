9e4fb06ca811b2122cea30b47183b1e8d5e7e0e0
s33127 <s33127@pjwstk.edu.pl>
2025-04-17 10:30:22 +0200

    auto

--- car.java
+++ car.java
@@ -1,2 +1,8 @@
-public class car {
+public class car extends vehicle {
+
+    private int numberOfSeats;
+
+    public void getNumberOfSeats() {
+        System.out.println("Number of Seats in Car: " + numberOfSeats);
+    }
 }
